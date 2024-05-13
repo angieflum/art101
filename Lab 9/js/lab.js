e779a33d0d6023d2f79fde0fe354c8e5ba57f10b
angieflum <aflum@ucsc.edu>
2024-05-12 20:06:46 -0700

    pls work

--- Lab 9/js/lab.js
+++ Lab 9/js/lab.js
@@ -3,17 +3,35 @@ Lab ^ JavaScript program
 Angie and Artem 
 */
 /*
+/*
 lab.js - This simple JavaScript/jQuery script uses buttons to modify some elements on the page
-
-   Requirements: jQuery must be loaded for this script to work.
-
-   Author: Angie and Artem
-   Date: May 12, 2024
+Requirements: jQuery must be loaded for this script to work.
+Author: Wes Modes
+Date: 2023
 */
-// add button to challenge section
-$("#challenge").append("<button id='button-challenge'>Make Special</button>");
-// add a click listener to the challenge button
-$("button-challenge").click(function(){
-    // now add (or subtract) the "special" class to the section
-    $("#challenge").toggleClass("special");
+
+$(document).ready(function() {
+    // add button to challenge section
+    $("#challenge").append("<button id='button-challenge'>Make Special</button>");
+
+    // add button to problems section
+    $("#problems").append("<button id='button-problems'>Make Special</button>");
+
+    // add button to results section
+    $("#results").append("<button id='button-results'>Make Special</button>");
+
+    // add click listener to the challenge button
+    $("#button-challenge").click(function() {
+        $("#challenge").toggleClass("special");
+    });
+
+    // add click listener to the problems button
+    $("#button-problems").click(function() {
+        $("#problems").toggleClass("special");
+    });
+
+    // add click listener to the results button
+    $("#button-results").click(function() {
+        $("#results").toggleClass("special");
+    });
 });
