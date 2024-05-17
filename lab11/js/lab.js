8616b448b7aac517c94c41eb45509fdeb72cb5eb
angieflum <aflum@ucsc.edu>
2024-05-17 15:13:13 -0700

    string

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -1,25 +1,26 @@
-/* lab.js 
-   This simple JavaScript/jQuery script appends new elements to an output div
-   Requirements:
-   jQuery must be loaded for this script to work.
-   Author: Angie and Artem
-   Date: May 17, 2024
+/* 
+lab.js - This simple JavaScript/jQuery script gets a value from an input field and outputs a modified version.
+
+Requirements: jQuery must be loaded for this script to work.
+
+Author: Angie and Artem
+Date: 17 May, 2024
 */
 
-// Function to generate random text
-function generateRandomText() {
-  const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
-  const min = 3;
-  const max = 100;
-  const randLen = Math.floor(Math.random() * (max - min + 1)) + min;
-  const randStart = Math.floor(Math.random() * (text.length - randLen + 1));
-  return text.slice(randStart, randStart + randLen);
+// Sorts the characters of a string in alphabetical order.
+function sortString(inputString) {
+  // We have to convert our string to an array and back again to sort it
+  return inputString.split('').sort().join('');
 }
 
-// Event listener for button click
-$("#make-convo").click(function(){
-  // Get new fake dialogue
-  const newText = generateRandomText();
+// Click listener for button
+$("#submit").click(function() {
+  // Get value of input field
+  const userName = $("#user-name").val();
+  
+  // Now let's sort it
+  const userNameSorted = sortString(userName);
+  
   // Append a new div to our output div
-  $("#output").append('<div class="text"><p>' + newText + '</p></div>');
+  $("#output").html('<div class="text"><p>' + userNameSorted + '</p></div>');
 });
